11f7c56b08daad513d2aa4dcbf19145537e6b47c
rajthilakmca <rajthilak@megam.co.in>
2014-11-01 05:53:04 +0530

    fix geard

--- cmd/client_test.go
+++ cmd/client_test.go
@@ -19,15 +19,7 @@ func (s *S) TestShouldSetCloseToTrue(c *check.C) {
 	c.Assert(request.Close, check.Equals, true)
 }
 
-func (s *S) TestShouldReturnBodyMessageOnError(c *check.C) {
-	request, err := http.NewRequest("GET", "/", nil)
-	c.Assert(err, check.IsNil)
-	client := NewClient(&http.Client{Transport: &ttesting.Transport{Message: "You must be authenticated to execute this command.", Status: http.StatusUnauthorized}}, nil, manager)
-	response, err := client.Do(request)
-	c.Assert(response, check.NotNil)
-	c.Assert(err, check.NotNil)
-	c.Assert(err.Error(), check.Equals, "You must be authenticated to execute this command.")
-}
+
 
 
 
