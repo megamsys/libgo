118ee763d87c11f12da8ee2c4a85cbb114a245d1
indykish <nkishore@megam.io>
2015-11-09 15:23:09 +0530

    Added figuring out os to libgo

---
+++ os/os.go
@@ -0,0 +1,30 @@
+package os
+
+var HostOS = hostOS // for monkey patching
+
+type OSType int
+
+const (
+	Unknown OSType = iota
+	Ubuntu
+	Windows
+	OSX
+	CentOS
+	Arch
+)
+
+func (t OSType) String() string {
+	switch t {
+	case Ubuntu:
+		return "Ubuntu"
+	case Windows:
+		return "Windows"
+	case OSX:
+		return "OSX"
+	case CentOS:
+		return "CentOS"
+	case Arch:
+		return "Arch"
+	}
+	return "Unknown"
+}
