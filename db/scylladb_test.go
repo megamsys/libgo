8be4331300784281581bafce21d4291e9be15dd8
vijaykanthm28 <mvijaykanth@megam.io>
2016-03-10 11:23:11 +0530

    scylladb test changed

--- db/scylladb_test.go
+++ db/scylladb_test.go
@@ -22,11 +22,31 @@ type Customer struct {
 	Name string
 }
 
+type Customer2 struct {
+	Id   string
+	Name string
+	Age  int
+}
+
 var _ = check.Suite(&S{})
 
 var noips = []string{"103.56.92.24"}
 
 func (s *S) SetUpSuite(c *check.C) {
+	s.sy, _ = NewScyllaDB(ScyllaDBOpts{
+		KeySpaceName: "testing",
+		NodeIps:      noips,
+		Username:     "",
+		Password:     "",
+		Debug:        true,
+	})
+	c.Assert(s.sy, check.NotNil)
+
+	if s.sy == nil {
+		fmt.Println("------------- scylladb is not running")
+		c.Skip("- ScyllaDB isn't running. Did you start it ? ")
+	}
+
 	cluster := gocql.NewCluster("103.56.92.24")
 	cluster.Keyspace = "testing"
 	cluster.Consistency = gocql.Quorum
@@ -42,17 +62,17 @@ func (s *S) SetUpSuite(c *check.C) {
 
 func (s *S) TestReadWhereRowNotFound(c *check.C) {
 	rand.Seed(time.Now().Unix())
-	t := s.sy.Table("customer", []string{"Id","Name"}, []string{}, &Customer{})
+	t := s.sy.Table("customer2", []string{"Id","Name"}, []string{}, &Customer2{})
 	err := t.T.(gocassa.TableChanger).CreateIfNotExist()
 	c.Assert(err, check.IsNil)
-	err = t.Upsert(&Customer{
+	err = t.Upsert(&Customer2{
 		Id:   "1001",
 		Name: "Hari",
 		Age: 26,
 	})
 	c.Assert(err, check.IsNil)
-	res := Customer{}
-	err = t.ReadWhere(ScyllaWhere{Clauses: map[string]string{"Id": "1001", "Name": ""}}, &res)
+	res := &Customer2{}
+	err = t.ReadWhere(ScyllaWhere{clauses: map[string]string{"Id": "1001", "Name": ""}}, res)
 	c.Assert(err, check.NotNil)
 }
 func (s *S) TestTablWithMultiplePKButReadUsingOnePK(c *check.C) {
@@ -80,6 +100,11 @@ func (s *S) TestReadWhereRowFound(c *check.C) {
 		Name: "Joe",
 	})
 	c.Assert(err, check.IsNil)
+
+	res := &Customer{}
+	err = t.ReadWhere(ScyllaWhere{clauses: map[string]string{"Id": "1001", "Name": "Joe"}}, res)
+	c.Assert(err, check.NotNil)
+}
 	res := Customer{}
 	err = t.ReadWhere(ScyllaWhere{Clauses: map[string]string{"Id": "1001", "Name": "Joe"}}, &res)
 	c.Assert(err, check.IsNil)
